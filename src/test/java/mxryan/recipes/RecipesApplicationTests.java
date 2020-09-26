d1e93417ac6664563002990281ffc648b4ea2ed4
Michael Ryan <mxryan@live.com>
2020-09-26 10:01:27 -0400

    initial commit

---
+++ src/test/java/mxryan/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package mxryan.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
